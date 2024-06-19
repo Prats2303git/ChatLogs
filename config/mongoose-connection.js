19c89d1b6b7d44953e3e4c49a2fce261005d04d7
Pratyai Chakrabarty <pratyai916@gmail.com>
2024-06-19 17:31:07 +0530

    changes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose
+async () => {
+await mongoose
 .connect(process.env.MONGODB_URI)
 .then(() => {
     console.log("connected");
 })
 .catch((err) => {
     console.log(err.message);
-})
+})}
 module.exports = mongoose.connection;
